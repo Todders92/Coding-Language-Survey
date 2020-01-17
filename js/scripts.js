7a989831e7d654898d221193f37d4aa09271a6ff
Todd Walraven <cod4247@yahoo.com>
2020-01-17 09:36:06 -0800

    setup up variables in javascript to give values to answers from survey

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,19 @@
 //below this line is back end logic
 
 //below this line is front end logic
+$(document).ready(function() {
+  $("form#quiz").submit(function(event) {
+    event.preventDefault();
+    var q1 = parseInt($("input:radio[name=bear]:checked").val());
+    var q2 = parseInt($("input:radio[name=problem]:checked").val());
+    var q3 = parseInt($("input:radio[name=problem]:checked").val());
+    var q4 = parseInt($("input:radio[name=problem]:checked").val());
+    var q5 = parseInt($("input:radio[name=problem]:checked").val());
+    var q6 = parseInt($("input:radio[name=problem]:checked").val());
+    var q7 = parseInt($("input:radio[name=problem]:checked").val());
+    var q8 = parseInt($("input:radio[name=problem]:checked").val());
+    var q9 = parseInt($("input:radio[name=problem]:checked").val());
+    var q10 = parseInt($("input:radio[name=problem]:checked").val());
+    var score = q1 + q2 + q3 + q4 + q5 + q6 + q7 + q8 + q9 +q10
+  });
+});
