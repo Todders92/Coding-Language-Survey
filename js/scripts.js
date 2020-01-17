55dce04232ee68cef9a4ab89d7e28c84ed529342
Todd Walraven <cod4247@yahoo.com>
2020-01-17 11:02:13 -0800

    add error div, establish proper branching to properly return survey result for all score conditions.

--- js/scripts.js
+++ js/scripts.js
@@ -15,10 +15,62 @@ $(document).ready(function() {
     var q9 = parseInt($("input:radio[name=problem]:checked").val());
     var q10 = parseInt($("input:radio[name=problem]:checked").val());
     var score = q1 + q2 + q3 + q4 + q5 + q6 + q7 + q8 + q9 +q10
-    if (score >= 15) {
+    if (score >= 45) {
+      $(".ruby").show();
+      $(".csharp").hide();
+      $(".rust").hide();
+      $(".react").hide();
+      $(".python").hide();
+      $(".javascript").hide();
+      $(".error").hide();
+    } else if (score < 45 && score >= 38) {
+      $(".ruby").hide();
+      $(".csharp").show();
+      $(".rust").hide();
+      $(".react").hide();
+      $(".python").hide();
+      $(".javascript").hide();
+      $(".error").hide();
+    } else if (score < 38 && score >= 31) {
+      $(".ruby").hide();
+      $(".csharp").hide();
+      $(".rust").show();
+      $(".react").hide();
+      $(".python").hide();
+      $(".javascript").hide();
+      $(".error").hide();
+    } else if (score < 31 && score >= 24) {
+      $(".ruby").hide();
+      $(".csharp").hide();
+      $(".rust").hide();
+      $(".react").show();
+      $(".python").hide();
+      $(".javascript").hide();
+      $(".error").hide();
+    } else if (score < 24 && score >= 17) {
+      $(".ruby").hide();
+      $(".csharp").hide();
+      $(".rust").hide();
+      $(".react").hide();
       $(".python").show();
-    } else {
+      $(".javascript").hide();
+      $(".error").hide();
+    } else if (score < 17 && score >= 10) {
+      $(".ruby").hide();
+      $(".csharp").hide();
+      $(".rust").hide();
+      $(".react").hide();
+      $(".python").hide();
       $(".javascript").show();
-    }
+      $(".error").hide();
+    } else if (score < 10) {
+      $(".ruby").hide();
+      $(".csharp").hide();
+      $(".rust").hide();
+      $(".react").hide();
+      $(".python").hide();
+      $(".javascript").hide();
+      $(".error").show();
+    } 
   });
 });
